202c8ba6af09f1f7d67e227627abc056d62f2b3d
Koen Bonnet <koen@goodgrid.nl>
2024-06-10 20:29:58 +0200

    Test deployment at Render

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
-import secrets from './secrets.js'
-
 const config = {
     timezone: "Europe/Amsterdam",
     oktaInstances: {
         source: {
-            baseUrl: "https://dev-05729419.okta.com/api/v1",
-            token: secrets.sourceToken, //This is currently only for creation of test data!!
+            baseUrl: process.env.OKTA_SOURCE_BASEURL,
+            token: process.env.OKTA_SOURCE_TOKEN,
             allowedStatusses: ["SUCCESS", "MFA_ENROLL"]
         },
         target: {
-            baseUrl: "https://dev-4478738.okta.com/api/v1",
-            token: secrets.targetToken,
+            baseUrl: process.env.OKTA_SOURCE_BASEURL,
+            token: process.env.OKTA_TARGET_TOKEN,
             hookAuthentication: {
                 header: "authorization",
-                secret: secrets.targetSecret
+                secret: OKTA_TARGET_SECRET
             }
         }
     }
